ad56850adda15442884a4b003da4a6e2ea1bde92
Collin Campbell <campbellcl19@gcc.edu>
2022-03-22 17:41:37 -0400

    Akari board and gameplay done

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Author - Collin Campbell
  */
@@ -5,46 +7,235 @@ public class Board {
     public int size = 0; //size of the board size x size
     public Cell[][] board;
 
-    Board(int size, boolean walls[][], int numberBulb[][]){
+    Board(int size) {
         this.size = size;
         this.board = new Cell[size][size];
         initCells();
-        placeWalls(walls, numberBulb);
     }
 
-    void initCells(){
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                board[i][j] = new Cell();
+    /**
+     * init the cells into mem.
+     */
+    void initCells() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new Cell(i, j);
             }
         }
     }
 
     /**
-     * @param walls - Where the walls are placed on the board
+     * @param i      - row
+     * @param j      - col
+     * @param number - wall number (how many bulbs are around the wall)
      */
-    void placeWalls(boolean walls[][], int[][] numberBulb){
-        for(int i = 0; i < this.size; i++){
-            for(int j = 0; j < this.size; j++){
-                if(walls[i][j]){
-                    board[i][j].setWall(true);
-                    board[i][j].setBulbsAroundWall(numberBulb[i][j]);
-                }
+    void placeWall(int i, int j, int number) {
+        Cell position = board[i][j];
+        position.setBulbsAroundWall(number);
+        position.setWall(true);
+    }
+
+    void placeBulb(int i, int j) {
+        //place the bulb
+        boolean canPlace = board[i][j].checkCell() && checkNeighbors(i, j);;
+
+        //light up the tiles
+        if (canPlace) {
+            board[i][j].setBulb(true);
+            lightUpTiles(i, j);
+        }
+    }
+
+    /**
+     * @param i - row
+     * @param j - col
+     * @return if it can placed based on the wall constraint
+     */
+    private boolean checkNeighbors(int i, int j) {
+        ArrayList<Cell> walls = new ArrayList<>();
+
+        if(i > 0 && i < 6 && j > 0 && j < 6) {
+            //check above
+            if (board[i - 1][j].getWall()) {
+                walls.add(board[i - 1][j]);
             }
+            //check below
+            if (board[i + 1][j].getWall()) {
+                walls.add(board[i + 1][j]);
+            }
+            //check left
+            if (board[i][j - 1].getWall()) {
+                walls.add(board[i - 1][j]);
+            }
+            //check right
+            if (board[i][j + 1].getWall()) {
+                walls.add(board[i + 1][j]);
+            }
+        }else{ //edge cases
+            //if we're at the top left corner
+            if(i == 0 && j == 0){
+                //check below
+                if (board[i + 1][j].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+                //check right
+                if (board[i][j + 1].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+            }else if(i == 6 && j == 0){ //we're at the bottom left corner
+                //check above
+                if (board[i - 1][j].getWall()) {
+                    walls.add(board[i - 1][j]);
+                }
+                //check right
+                if (board[i][j + 1].getWall()) {
+                    walls.add(board[i][j + 1]);
+                }
+            }else if(i == 0 && j == 6){ //at the top right corner
+                //check below
+                if (board[i + 1][j].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+                //check left
+                if (board[i][j - 1].getWall()) {
+                    walls.add(board[i][j - 1]);
+                }
+            }else if(i == 6 && j == 6){//bottom right corner
+                //check above
+                if (board[i - 1][j].getWall()) {
+                    walls.add(board[i - 1][j]);
+                }
+                //check left
+                if (board[i][j - 1].getWall()) {
+                    walls.add(board[i][j - 1]);
+                }
+            }else if(i == 0){ //were at the top most corner
+                //check below
+                if (board[i + 1][j].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+                //check left
+                if (board[i][j - 1].getWall()) {
+                    walls.add(board[i][j - 1]);
+                }
+                //check right
+                if (board[i][j + 1].getWall()) {
+                    walls.add(board[i][j + 1]);
+                }
+            }else if(i == 6){ //were at the right most corner
+                //check above
+                if (board[i - 1][j].getWall()) {
+                    walls.add(board[i - 1][j]);
+                }
+                //check left
+                if (board[i][j - 1].getWall()) {
+                    walls.add(board[i][j - 1]);
+                }
+                //check right
+                if (board[i][j + 1].getWall()) {
+                    walls.add(board[i][j + 1]);
+                }
+            }else if(j == 0){ //were at left most corner
+                //check above
+                if (board[i - 1][j].getWall()) {
+                    walls.add(board[i - 1][j]);
+                }
+                //check below
+                if (board[i + 1][j].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+                //check right
+                if (board[i][j + 1].getWall()) {
+                    walls.add(board[i][j + 1]);
+                }
+            }else if(j == 6){ //at the most right most corner
+                //check above
+                if (board[i - 1][j].getWall()) {
+                    walls.add(board[i - 1][j]);
+                }
+                //check below
+                if (board[i + 1][j].getWall()) {
+                    walls.add(board[i + 1][j]);
+                }
+                //check left
+                if (board[i][j - 1].getWall()) {
+                    walls.add(board[i][j - 1]);
+                }
+            }else{
+                System.out.print("WE SHOULD NEVER GET HERE");
+            }
+        }
+
+
+        //checking if the bulb is allowed to be there
+        for (int b = 0; b < walls.size(); b++) {
+            Cell curr = walls.get(b);
+            if (curr.bulbs <= 0) {
+                System.out.println("Cant place a bulb here due to wall constriction");
+                return false;
+            }
+        }
+
+        //if so decrement all the walls bulbs count
+        for (int b = 0; b < walls.size(); b++) {
+            Cell curr = walls.get(b);
+            curr.bulbs--;
+        }
+
+        return true;
+    }
+
+    private void lightUpTiles(int i, int j) {
+        //up
+        for (int z = i; z >= 0; z--) {
+            if (board[z][j].getWall()) {
+                break;
+            }
+            board[z][j].setLit(true);
+        }
+
+        //down
+        for (int z = i; z < size; z++) {
+            if (board[z][j].getWall()) {
+                break;
+            }
+            board[z][j].setLit(true);
+        }
+
+        //right
+        for (int z = j; z < size; z++) {
+            if (board[i][z].getWall()) {
+                break;
+            }
+            board[i][z].setLit(true);
+        }
+
+        //left
+        for (int z = j; z >= 0; z--) {
+            if (board[i][z].getWall()) {
+                break;
+            }
+            board[i][z].setLit(true);
         }
     }
 
     /**
      * Prints out the board at any given time
      */
-    void printBoard(){
-        for(int i = 0; i < this.size; i++){
-            for(int j = 0; j < this.size; j++){
-                if(this.board[i][j].wall){
-                    System.out.print(" X" + this.board[i][j].bulbsAroundWall);
-                }else if(this.board[i][j].lit){
-                    System.out.println(" L ");
-                }else{
+    void printBoard() {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                if (this.board[i][j].getWall()) {
+                    if (board[i][j].getBulbsAroundWall() != -1) {
+                        System.out.print(" X" + this.board[i][j].bulbsAroundWall);
+                    } else {
+                        System.out.print(" X ");
+                    }
+                } else if (this.board[i][j].getBulb()) {
+                    System.out.print(" B ");
+                } else if (this.board[i][j].getLit()) {
+                    System.out.print(" L ");
+                } else {
                     System.out.print(" - ");
                 }
             }
@@ -52,87 +243,142 @@ public class Board {
         }
     }
 
-    class Cell{
-        boolean wall = false;
-        boolean lightBulb = false;
-        boolean lit = false;
-        int bulbsAroundWall = 0;
+    /**
+     * Used for debugging the win condition
+     */
+//    void printBoardCheck(){
+//        for(int i = 0; i < size; i++){
+//            for(int j = 0; j < size; j++){
+//                if(board[i][j].getWall() || board[i][j].getLit() ){
+//                    System.out.print(" T ");
+//                }else{
+//                    System.out.print(" F ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
 
-        Cell(boolean wall, boolean lightBulb, boolean lit){
+    /**
+     * @return if the puzzle has been solved
+     */
+    boolean checkSolved() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j].getWall() || board[i][j].getLit()) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    class Cell {
+        private boolean wall = false;
+        private boolean lightBulb = false;
+        private boolean lit = false;
+        private int bulbsAroundWall = 0;
+        private int row;
+        private int col;
+        private int bulbs = 0;
+
+        Cell(boolean wall, boolean lightBulb, boolean lit) {
             this.wall = wall;
             this.lightBulb = lightBulb;
             this.lit = lit;
         }
 
-        Cell(){
-            this.wall = false;
-            this.lightBulb = false;
-            this.lit = false;
+        Cell(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
 
         /**
          * Sets it if the cell is lit or not
-         * @param lit
+         *
+         * @param lit t/f
          */
-        void setLit(boolean lit){
+        void setLit(boolean lit) {
             this.lit = lit;
         }
 
         /**
          * returns whether or not the cell is lit
+         *
          * @return
          */
-        boolean getLit(){
+        boolean getLit() {
             return lit;
         }
 
         /**
          * Sets a bulb in that cell
+         *
          * @param bulb
          */
-        void setBulb(boolean bulb){
+        void setBulb(boolean bulb) {
             this.lightBulb = bulb;
         }
 
+        boolean checkCell(){
+            if (wall || lit) {
+                System.out.println("Cant place a bulb here: (" + row + ", " + col + ")");
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         *
+         * @return if the bulb can be placed, under the wall constraint
+         */
+
         /**
          * Returns if there is a lightbulb in the current cell
+         *
          * @return
          */
-        boolean getBulb(){
+        boolean getBulb() {
             return lightBulb;
         }
 
         /**
          * returns wheter the cell is a wall or not
+         *
          * @return
          */
-        boolean getWall(){
+        boolean getWall() {
             return this.wall;
         }
 
         /**
          * Sets the wall at the cell
+         *
          * @param wall
          * @return
          */
-        void setWall(boolean wall){
+        void setWall(boolean wall) {
             this.wall = wall;
         }
 
         /**
-         *
          * @return The number of bulbs for any wall
          */
-        int getBulbsAroundWall(){
+        int getBulbsAroundWall() {
             return this.bulbsAroundWall;
         }
 
         /**
          * Setting the number of bulbs for any wall
+         *
          * @param number
          */
-        void setBulbsAroundWall(int number){
+        void setBulbsAroundWall(int number) {
             this.bulbsAroundWall = number;
+            this.bulbs = number;
         }
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,67 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         /*
         TESTING BOARD
          */
-        int size = 7;
-        boolean[][] walls = new boolean[size][size];
-        int[][] numberBulbsAroundWall = new int[size][size];
-        walls[1][1] = true;
-        walls[5][2] = true;
-        numberBulbsAroundWall[1][1] = 2;
-        numberBulbsAroundWall[5][2] = 3;
 
-        Board board = new Board(size, walls, numberBulbsAroundWall);
+         /*Board board = new Board(7);
 
+         board.placeWall(5, 5, -1);
+         board.placeWall(3,4,3);
+
+         board.placeBulb(4, 3);
+         board.placeBulb(4,2);
+         board.placeBulb(5, 5);
+
+
+         board.printBoard();*/
+
+        //init scanner for user input
+        Scanner input = new Scanner(System.in);
+
+        //initial board taken from https://www.puzzle-light-up.com/faq.php
+        Board board = new Board(7); //7x7 board
+
+        //place walls
+        board.placeWall(0, 3, 1);
+        board.placeWall(0, 5, 0);
+        board.placeWall(1, 0, -1);
+        board.placeWall(2, 2, -1);
+        board.placeWall(2, 4, -1);
+        board.placeWall(3, 0, -1);
+        board.placeWall(3, 6, -1);
+        board.placeWall(4, 2, -1);
+        board.placeWall(4, 4, 3);
+        board.placeWall(5, 6, -1);
+        board.placeWall(6, 1, 3);
+        board.placeWall(6, 3, 2);
+
+        //print board
+        System.out.println("Initial board\n");
         board.printBoard();
+
+        int turns = 0;
+        while(!board.checkSolved()){
+            System.out.println("Turn: " + turns);
+            board.printBoard();
+            System.out.print("Input - i j - to place a bulb:");
+            try {
+                int i = Integer.parseInt(input.next());
+                int j = Integer.parseInt(input.next());
+
+                board.placeBulb(i, j);
+
+            }catch (Exception e){
+                System.out.println(e.toString());
+            }
+            turns++;
+        }
+
+
+        input.close();
     }
 
 }
